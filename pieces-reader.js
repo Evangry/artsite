58d322d885b23f9a36cc353b7f1b3a88c499ee42
Evan Geary <egeary@uw.edu>
2022-06-18 09:59:44 -0700

    display all data on site

--- pieces-reader.js
+++ pieces-reader.js
@@ -1,18 +1,19 @@
-const file = document.getElementById("fileContents").textContent;
-console.log(file);
-const lines = file.split('\n')
-console.log(lines)
-lines.forEach((content, index, array) => {
- if (content.toLowerCase().startsWith('project')){
- 		array[index] = content.split('ject: ')[1];
+const file = document.getElementById("file-contents").textContent;
+let displayHtml = ""
 
- 	} else if (content){
- 		let piece = content.split(', ')
- 		
- 		
-
- }
-
+const projects = file.split('\nProject: ').slice(1);
+projects.forEach((content, index, array) => {
+	let lines = content.split('\n');
+	let projectTitle = lines[0];
+	displayHtml += `<h3>${projectTitle}</h3>`;
+	let pieces = lines.slice(2, -1);
+	pieces.forEach((content, index, array) => {
+		let pieceData = content.split(', ');
+		console.log('piece title: ' + pieceData[0]);
+		displayHtml += `<h4>${pieceData[0]}</h4>`
+		displayHtml += `<img src='pieces/${pieceData[0]}.JPG' alt="image">`
+		displayHtml += `${pieceData[1]}, ${pieceData[2]}`
+	})
 })
 
-console.log(lines)
+document.getElementById("work-display").innerHTML = displayHtml
